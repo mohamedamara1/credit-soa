dc0a3ecdaef032174b49ac9c6d8db7c480935545
yassinMH <102417379+yassinMH@users.noreply.github.com>
2025-01-13 22:59:22 +0100

    Update ScoringController.java

--- scoring-service/src/main/java/org/example/scoringservice/controller/ScoringController.java
+++ scoring-service/src/main/java/org/example/scoringservice/controller/ScoringController.java
@@ -1,14 +1,11 @@
 package org.example.scoringservice.controller;
 
-
+import org.example.scoringservice.dto.ScoreDTO;
+import org.example.scoringservice.service.ScoringService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.example.scoringservice.service.ScoringService;
-import org.example.scoringservice.dto.ScoreDTO;
-import org.example.scoringservice.model.Score;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/scores")
@@ -19,51 +16,26 @@ public class ScoringController {
 
     @PostMapping
     public ScoreDTO createScore(@RequestBody ScoreDTO scoreDTO) {
-        Score score = convertToEntity(scoreDTO);
-        Score createdScore = scoringService.createScore(score);
-        return convertToDTO(createdScore);
+        return scoringService.createScore(scoreDTO);
     }
 
     @GetMapping("/{id}")
     public ScoreDTO getScore(@PathVariable String id) {
-        Score score = scoringService.getScore(id);
-        return convertToDTO(score);
+        return scoringService.getScore(id);
     }
 
     @GetMapping
     public List<ScoreDTO> getAllScores() {
-        List<Score> scores = scoringService.getAllScores();
-        return scores.stream().map(this::convertToDTO).collect(Collectors.toList());
+        return scoringService.getAllScores();
     }
 
     @PutMapping("/{id}")
     public ScoreDTO updateScore(@PathVariable String id, @RequestBody ScoreDTO scoreDTO) {
-        Score score = convertToEntity(scoreDTO);
-        Score updatedScore = scoringService.updateScore(id, score);
-        return convertToDTO(updatedScore);
+        return scoringService.updateScore(id, scoreDTO);
     }
 
     @DeleteMapping("/{id}")
     public void deleteScore(@PathVariable String id) {
         scoringService.deleteScore(id);
     }
-
-    private ScoreDTO convertToDTO(Score score) {
-        ScoreDTO scoreDTO = new ScoreDTO();
-        scoreDTO.setId(score.getId());
-        scoreDTO.setCreditId(score.getCreditId());
-        scoreDTO.setScore(score.getScore());
-        scoreDTO.setEvaluation(score.getEvaluation());
-        return scoreDTO;
-    }
-
-    private Score convertToEntity(ScoreDTO scoreDTO) {
-        Score score = new Score();
-        score.setId(scoreDTO.getId());
-        score.setCreditId(scoreDTO.getCreditId());
-        score.setScore(scoreDTO.getScore());
-        score.setEvaluation(scoreDTO.getEvaluation());
-        return score;
-    }
 }
-
